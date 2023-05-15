ab0cf0052fa06f5e62e28e9f3837ab6f21f3d7a7
Santiago Andres Coronado <santiicoronado.70@gmail.com>
2023-05-15 16:31:38 -0300

    cambio en las politicas CORS

--- src/main/java/portfolio/backend/config/CorsConfig.java
+++ src/main/java/portfolio/backend/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer(){
             @Override
             public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**");
                 registry.addMapping("/login").allowedOrigins("https://portfolio-frontend-santi.web.app").allowedMethods("*").exposedHeaders("*");
                 registry.addMapping("/api/**").allowedOrigins("https://portfolio-frontend-santi.web.app").allowedMethods("*");
             }
